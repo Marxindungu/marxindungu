deed545fac21715c52b8018d294ef43087eedf0c
eMobilis <marxinono@gmail.com>
2023-06-27 10:11:40 +0300

    betting

---
+++ betting.py
@@ -0,0 +1,20 @@
+#create a simple reward system
+#user any number 1 - 5
+#they are rewarded
+
+number = int(input("Enter any number"))
+if number == 1:
+  print("reward is a car")
+if number == 2:
+    print("reward is free vacation")
+if number == 3:
+    print("dinner with ariana grande")
+if number  == 4:
+    print("sorry no reward")
+if number == 5:
+    print("25 kshs")
+else:
+    print("sorry")
+
+
+
