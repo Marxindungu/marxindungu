1b17865b47075e16f402b1e376adc4a7695f69e2
eMobilis <marxinono@gmail.com>
2023-06-29 09:17:06 +0300

    bmi

--- BMI.py
+++ BMI.py
@@ -6,4 +6,17 @@ weight = float(weight)
 height = float(height)
 
 result = weight/(height**2)
-print("Your bmi is", result)
+print("Your BMI IS", result)
+
+if result < 18.0:
+   print("underweight")
+elif result <= 18.1:
+    print("Normal weight")
+elif result <= 29:
+    print("Normal weight")
+elif result <= 29.1:
+    print("Over weight")
+elif result <= 34:
+    print("Over weight")
+elif result > 34.1:
+    print("Obese")
