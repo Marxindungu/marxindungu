5c6d9fe148c78dc29a6ece450c96e07ef8724d90
eMobilis <marxinono@gmail.com>
2023-06-29 10:10:37 +0300

    DSA

--- Datastructures/DSA_SUMMARY.py
+++ Datastructures/DSA_SUMMARY.py
@@ -1,5 +1,12 @@
 #List
-    NOTES
-1...  Defined using square brackets{}
-  e.g.....
-   friends = {"Bob" , "Marie"}
+     NOTES
+1...  Defined using square brackets[]
+  e.g..... =
+           freinds = ["Bob" , "Marie"]
+2..can be modified
+3..has order
+
+#Tuples
+ 1..defined by normal brackets()
+ 2..you cannot modify a tuple
+ 3..
