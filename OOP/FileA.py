d979cfc28860f5a0948158c54891bd4668e27ba8
eMobilis <marxinono@gmail.com>
2023-07-06 09:03:52 +0300

    FileB

--- OOP/FileA.py
+++ OOP/FileA.py
@@ -1,3 +1,12 @@
 class ParentClass:
     def parentClassMethod(self):
         print("Im the parent and i have been inherited")
+    def addnums(self, a, v):
+        result = a + v
+        print(result)
+    def multinums(self, x, y):
+        result = x * y
+        print(result)
+    def divnums(self,a, b):
+        result = a / b
+        print(result)
--- OOP/FileB.py
+++ OOP/FileB.py
@@ -2,3 +2,7 @@ from FileA import ParentClass
 
 obj = ParentClass()
 obj.parentClassMethod()
+
+obj.addnums(15,5)
+obj.multinums(2,4)
+obj.divnums(8,4)
