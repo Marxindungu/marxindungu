66b66e6cb4c9acd1b7b7d67d59a2b8999024024b
eMobilis <marxinono@gmail.com>
2023-07-11 09:13:26 +0300

    Encapsulation

---
+++ OOP/polyEx2.py
@@ -0,0 +1,30 @@
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def move(self):
+            print("Driving around")
+
+class Plane:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+    def move(self):
+            print("Fly around")
+
+class Motorbike:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+    def move(self):
+            print("Ride around")
+
+#instance of our class
+car = Car("Note", "Satrit")
+plane = Plane("Boeing", "737")
+bike = Motorbike("Kawasaki", "Ninja650")
+
+for i in (car, plane, bike):
+    i.move()
+
---
+++ OOP/polymorphism.py
@@ -0,0 +1,12 @@
+y = "Heloo my people"
+print(len(y))
+
+student = ("Anne", "Jim", "Freddy")
+print(len(student))
+
+myvehicles = {
+    "brand": "Nissan",
+    "model": "Tean",
+    "Y.O.M": 2015
+}
+print(len(myvehicles))
